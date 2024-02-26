e2622771bcbeda0d6f58fa1c98ed791f38ac188e
김치헌 <chk87a@gmail.com>
2024-02-26 13:01:14 +0900

    <Feat> Camera Controller

--- Assets/Scripts/Controllers/VirtualCameraController.cs
+++ Assets/Scripts/Controllers/VirtualCameraController.cs
@@ -1,43 +1,135 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
 
 public class VirtualCameraController : MonoBehaviour
 {
+    public static VirtualCameraController instace; 
     public Transform v_CameraTarget;
+    public Transform playerTransform;
+    public CinemachineVirtualCamera VirtualCamera;
     CinemachineBrain brain;
     Camera mainCamera;
-    bool isCameraOn;
+    public bool isVirtualCameraOn;
+    Vector3 playerFoward;
+    Vector3 cameraFoward;
+    Vector3 crossProduct;
+    Vector3 targetCameraPos;
+    Vector3 cameraRot;
+    Quaternion targetCameraRot;
+    float cameraPos_x;
+    float cameraPos_z;
+    float dotProduct;
+    float angle;
+    float rad;
 
     private void Awake()
     {
+        instace = this;
         v_CameraTarget = GetComponent<Transform>();
+        VirtualCamera = GetComponent<CinemachineVirtualCamera>();
         brain = GetComponent<CinemachineBrain>();
         mainCamera = Camera.main;
-        isCameraOn = false;
+        isVirtualCameraOn = false;
     }
 
     private void FixedUpdate()
     {
-        transform.position = v_CameraTarget.transform.position;
 
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1)||Input.GetMouseButton(0))
         {
-
             brain.enabled = true;
-            isCameraOn = true;
+            isVirtualCameraOn = true;
         }
-
         else
         {
             brain.enabled = false;
-            isCameraOn = false;
+            isVirtualCameraOn = false;
         }
+        //MoveCameraBackward();
+    }
+    public void MoveCameraBackward()
+    {
+        if(!isVirtualCameraOn) 
+        {
+            playerFoward = playerTransform.forward;
+            cameraFoward = playerTransform.position-transform.position;
+            cameraFoward.y = 0;
+            playerFoward.y = 0;
+            rad = cameraFoward.magnitude;
+            crossProduct = Vector3.Cross(cameraFoward.normalized, playerFoward.normalized);
+            Debug.Log("Cross: " + crossProduct);
+            dotProduct = Vector3.Dot(cameraFoward.normalized, playerFoward.normalized);
+            Debug.Log("Dot: " + dotProduct);
+            angle =Vector3.Angle(cameraFoward.normalized, playerFoward.normalized)*(crossProduct.y>=0?1:-1);
+            Debug.Log("Angle: " + angle);
+            if (dotProduct < 0)
+            {
+                if(crossProduct.y < 0)
+                {
+                    angle = angle + 2;
+                    targetCameraPos.x = rad * Mathf.Sin(angle*Mathf.Deg2Rad);
+                    targetCameraPos.y = transform.localPosition.y;
+                    targetCameraPos.z = -rad * Mathf.Cos(angle*Mathf.Deg2Rad);
+                    cameraRot = transform.localRotation.eulerAngles;
+                    targetCameraRot = Quaternion.Euler(cameraRot.x, -angle, 0);
+                    transform.localPosition = targetCameraPos;
+                    transform.localRotation = targetCameraRot;
+                }
+                else
+                {
+                    angle = angle - 2;
+                    targetCameraPos.x = rad * Mathf.Sin(angle * Mathf.Deg2Rad);
+                    targetCameraPos.y = transform.localPosition.y;
+                    targetCameraPos.z = -rad * Mathf.Cos(angle * Mathf.Deg2Rad);
+                    cameraRot = transform.localRotation.eulerAngles;
+                    targetCameraRot = Quaternion.Euler(cameraRot.x, -angle, 0);
+                    transform.localPosition = targetCameraPos;
+                    transform.localRotation = targetCameraRot;
+                }
+                
+            }
+            else if(dotProduct>=0&&dotProduct<0.98f)
+            {
+                if (crossProduct.y < 0)
+                {
+                    angle = angle + 2;
+                    targetCameraPos.x = rad * Mathf.Sin(angle * Mathf.Deg2Rad);
+                    targetCameraPos.y = transform.localPosition.y;
+                    targetCameraPos.z = -rad * Mathf.Cos(angle * Mathf.Deg2Rad);
+                    cameraRot = transform.rotation.eulerAngles;
+                    targetCameraRot = Quaternion.Euler(cameraRot.x, -angle, 0);
+                    transform.localPosition = targetCameraPos;
+                    transform.localRotation = targetCameraRot;
+                }
+                else
+                {
+                    angle = angle - 2;
+                    targetCameraPos.x = rad * Mathf.Sin(angle * Mathf.Deg2Rad);
+                    targetCameraPos.y = transform.localPosition.y;
+                    targetCameraPos.z = -rad * Mathf.Cos(angle * Mathf.Deg2Rad);
+                    cameraRot = transform.localRotation.eulerAngles;
+                    targetCameraRot = Quaternion.Euler(cameraRot.x, -angle, 0);
+                    transform.localPosition = targetCameraPos;
+                    transform.localRotation = targetCameraRot;
+                }
+            }
+            else
+            {
+                angle = 0;
+                targetCameraPos.x = rad * Mathf.Sin(angle * Mathf.Deg2Rad);
+                targetCameraPos.y = transform.localPosition.y;
+                targetCameraPos.z = -rad * Mathf.Cos(angle * Mathf.Deg2Rad);
+                cameraRot = transform.localRotation.eulerAngles;
+                targetCameraRot = Quaternion.Euler(cameraRot.x, -angle, 0);
+                transform.localPosition = targetCameraPos;
+                transform.localRotation = targetCameraRot;
 
-
-
+            }
+        }
     }
 
 }
