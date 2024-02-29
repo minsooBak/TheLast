2d98b462146fde714c664876c4d9e5bda1b1a3ad
김치헌 <chk87a@gmail.com>
2024-02-29 14:08:21 +0900

    <Feat> SceneData

--- Assets/Scripts/Scenes/CharaterSelectScene.cs
+++ Assets/Scripts/Scenes/CharaterSelectScene.cs
@@ -1,8 +1,12 @@
 using Enums;
+using System.Collections.Generic;
 
 public class CharaterSelectScene : IBaseScene
 {
+    public int[] slotIndex = new int[3];
+    public Dictionary<int, CharacterLoadData> loadData;
     public override void Init()
     {
+        
     }
 }
