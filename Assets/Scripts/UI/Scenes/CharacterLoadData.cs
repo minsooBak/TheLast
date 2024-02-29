c4a12c3f060a424e0e968545fb2508921781e7b2
김치헌 <chk87a@gmail.com>
2024-03-01 01:29:44 +0900

    <Fix> CharacterSelectScene Controller

--- Assets/Scripts/UI/Scenes/CharacterLoadData.cs
+++ Assets/Scripts/UI/Scenes/CharacterLoadData.cs
@@ -1,18 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class CharacterLoadData : MonoBehaviour
+public class CharacterLoadData
 {
-    // Start is called before the first frame update
-    void Start()
+    public UserDataList userDataList;
+    public Dictionary<int, UserDataList> loadedUserData;
+
+    public void Init()
     {
-        
+        loadedUserData = new(3);
+        //Utility.LoadJsonFile<UserDataList>("UsersData.json");
+        LoadUserData();
+
+    }
+    [ContextMenu("To Json Data")]
+    public void SaveUserData()
+    {
+        string jsonData = JsonUtility.ToJson(userDataList, true);
+        string path = Path.Combine(Application.dataPath, $"UserData.json");
+        File.WriteAllText(path, jsonData);
+        Debug.Log("데이터 저장했음");
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("From Json Data")]
+    private void LoadUserData()
     {
-        
+        string path = Path.Combine(Application.dataPath, $"UserData.json");
+        if (!File.Exists(path))
+        {
+            Debug.Log("No File");
+            //userDataList = new UserDataList();
+            return;
+        }
+        string jsonData = File.ReadAllText(path);
+
+        userDataList = JsonUtility.FromJson<UserDataList>(jsonData);
+        Debug.Log("데이터 불러왔음");
     }
 }
+[System.Serializable]
+public class UserData
+{
+    public int characterSlot;
+    public byte statusId;
+    public string characterName;
+    public int Level;
+    public int exp;
+    public int stageLv;
+    //플레이어인벤토리,스킬업글,스텟업글
+}
+public class PlayData
+{
+
+}
+
+[System.Serializable]
+public class UserDataList
+{
+    public List<UserData> user = new List<UserData>();
+}
+
+
+
+
--- Assets/Scripts/UI/Scenes/CharacterSelectUI.cs
+++ Assets/Scripts/UI/Scenes/CharacterSelectUI.cs
@@ -1,8 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelectUI : MonoBehaviour
 {
-    
+    CharacterDefaultData characterDefaultData;
+    CharacterLoadData characterLoadData;
+    [SerializeField] private List<TextMeshProUGUI> slotText1;
+    [SerializeField] private List<TextMeshProUGUI> slotText2;
+    [SerializeField] private List<TextMeshProUGUI> slotText3;
+    [SerializeField] private List<GameObject> characterSlot;
+    [SerializeField] private List<GameObject> emptySlot;
+    [SerializeField] private List<Image> jobIcon;
+
+    private void Awake()
+    {
+        characterDefaultData = new CharacterDefaultData();
+        characterLoadData = new CharacterLoadData();
+        characterDefaultData.Init();
+        characterLoadData.Init();
+    }
+    private void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        DisplaySelectScene();
+    }
+    private void DisplaySelectScene()
+    {
+        for(int i = 0; i < 3; i++) 
+        {            
+            if (characterLoadData.userDataList != null)
+            {
+                UserData userData = characterLoadData.userDataList.user[i];
+                characterSlot[i].SetActive(true);
+                emptySlot[i].SetActive(false);
+                if (i==0) 
+                { 
+                    slotText1[0].text = userData.Level.ToString();
+                    slotText1[1].text = (userData.statusId==(byte)1 ? "마법사" : "오크전사");
+                    slotText1[2].text = userData.characterName.ToString();
+                    jobIcon[i] = (userData.statusId == (byte)1 ? Resources.Load<Image>("UI/Icon/UI/wizard") : Resources.Load<Image>("UI/Icon/UI/orc"));
+                }
+                if (i == 1)
+                {
+                    slotText2[0].text = userData.Level.ToString();
+                    slotText2[1].text = (userData.statusId == (byte)1 ? "마법사" : "오크전사");
+                    slotText2[2].text = userData.characterName.ToString();
+                    jobIcon[i] = (userData.statusId == (byte)1 ? Resources.Load<Image>("UI/Icon/UI/wizard") : Resources.Load<Image>("UI/Icon/UI/orc"));
+                }
+                if (i == 2)
+                {
+                    slotText3[0].text = userData.Level.ToString();
+                    slotText3[1].text = (userData.statusId == (byte)1 ? "마법사" : "오크전사");
+                    slotText3[2].text = userData.characterName.ToString();
+                    jobIcon[i] = (userData.statusId == (byte)1 ? Resources.Load<Image>("UI/Icon/UI/wizard") : Resources.Load<Image>("UI/Icon/UI/orc"));
+                }
+            }
+            else
+            {
+                characterSlot[i].SetActive(false);
+                emptySlot[i].SetActive(true);
+            }
+        }
+    }
+
 }
