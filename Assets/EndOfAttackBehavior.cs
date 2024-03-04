d945c65c68b7351a57d1d5bd5f1766b5a2498d05
gunwonpark <parkgunwon1@naver.com>
2024-03-04 23:19:08 +0900

    <fea> 몬스터 공격 적용

--- Assets/EndOfAttackBehavior.cs
+++ Assets/EndOfAttackBehavior.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class EndOfAttackBehavior : StateMachineBehaviour
 {
+    Enemy enemy;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemy = animator.GetComponent<Enemy>();
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float progress = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        if(!enemy.attackCollider.enabled && progress > 0.3f)
+        {
+            enemy.EnableAttackCollider();
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Enemy enemy = animator.GetComponent<Enemy>();
+        enemy.DisableAttackCollider();
         enemy.StateMachine.ChangeState(enemy.StateMachine.IdleState);
     }
 
